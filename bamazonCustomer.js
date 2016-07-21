02d7a739c1116d1ff4266236dc5412e85f2eab77
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-07-21 12:54:02 -0400

    Display Product function added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var prompt = require('prompt');
 var mysql = require('mysql');
+var table = require('easy-table');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -15,12 +16,25 @@ connection.connect(function(err){
 
 function displayItems(){
   connection.query('Select * FROM products', function(err, res){
-    if(err) throw err;
-    console.log('Item ID        Product        Department      Price   Quantity');
-    for (i=0;i<res.length;i++){
+    // if(err) throw err;
+    // console.log('Item ID        Product        Department      Price   Quantity');
+    // for (i=0;i<res.length;i++){
       
-      console.log(JSON.stringify(res[i].ItemId + '  ' + res[i].ProductName + '  ' + res[i].DepartmentName + '  ' + res[i].Price + '  ' + res[i].StockQuantity));
-    }
+    //   console.log(JSON.stringify(res[i].ItemId + '  ' + res[i].ProductName + '  ' + res[i].DepartmentName + '  ' + res[i].Price + '  ' + res[i].StockQuantity));
+    // }
+    var t = new table;
+ 
+    res.forEach(function(product) {
+      t.cell('Product Id', product.ItemId)
+      t.cell('Description', product.ProductName)
+      t.cell('Department', product.DepartmentName)
+      t.cell('Price, USD ($)', product.Price, table.number(2))
+      t.cell('Quantity', product.StockQuantity)
+      t.newRow()
+    })
+
+
+    console.log(t.toString());
   })
 }
 
